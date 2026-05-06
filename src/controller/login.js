ed546c0737ed0ada6dd9a93b9d0c600f3e1df59e
suresh <sj3795789@gmail.com>
2026-05-06 16:53:35 +0530

    implemt login api

---
+++ src/controller/login.js
@@ -0,0 +1,70 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authSchema = require("../moduel/auth");
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "All fields are required",
+            });
+        }
+
+        // check user exist
+        const isMatch = await authSchema.findOne({ email });
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        // hash password
+        const comparePassword = await bcrypt.compare(password, isMatch.password);
+        if (!comparePassword) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+
+
+        // token generate
+        const token = jwt.sign(
+            { id: isMatch._id, email: isMatch.email },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        res.status(200).json({
+            message: "User login successfully",
+            token,
+            user: {
+                id: isMatch._id,
+                name: isMatch.name,
+                email: isMatch.email,
+            },
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Server error",
+        });
+    }
+};
+
+
+const getAllUser = async (req, res) => {
+    
+   try {
+     const user =  await authSchema.find().lean();
+     res.status(200).json({success:true,message:"user fetch",user})
+   } catch (error) {
+     res.status(500).json({
+            message: "Server error",
+        });
+   }
+}
+
+module.exports = { login,getAllUser };
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { signup } = require("../controller/sign");
+const { login, getAllUser } = require("../controller/login");
 const router = express.Router();
 
 router.post("/auth/signup", signup);
+router.post("/auth/login", login);
+router.get("/user", getAllUser);
 
 module.exports = router;
